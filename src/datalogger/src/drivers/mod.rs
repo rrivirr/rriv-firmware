a8024281519b4621ba34e056e1cfd144fef0661e
aneeshkandi14 <aneeshkandi@gmail.com>
2025-11-14 16:58:44 -0500

    fix: fixed field mode operation and added feature

--- src/datalogger/src/drivers/mod.rs
+++ src/datalogger/src/drivers/mod.rs
@@ -3,14 +3,21 @@
 // codes and sensor names mapped to a sensor implementation
 
 pub mod types;
+
 pub mod mcp9808;
 pub mod generic_analog;
 pub mod ring_temperature;
 pub mod timed_switch_2;
+#[cfg(not(feature = "N2O"))]
 pub mod ds18b20;
+#[cfg(not(feature = "N2O"))]
 pub mod k30_co2;
+#[cfg(not(feature = "N2O"))]
 pub mod atlas_ec;
+#[cfg(not(feature = "N2O"))]
 pub mod aht20;
+#[cfg(feature = "N2O")]
 pub mod basic_evo;
+
 pub mod resources;
 
