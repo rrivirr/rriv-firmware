9566989740086d40b0ceafb6cd25931bd2d02254
Zaven Arra <zaven.arra@gmail.com>
2026-02-17 12:37:47 -0800

    fix: Comment out the incomplete uart5 serial for now

--- board/rriv_board_0_4_2/src/lib.rs
+++ board/rriv_board_0_4_2/src/lib.rs
@@ -1535,6 +1535,8 @@ impl BoardBuilder {
 
         self.watchdog = Some(watchdog);
 
+        // defmt::println!("setting up RS485 serial b");
+        // setup_serialb(device_peripherals.UART5, &clocks);
 
         self.clocks = Some(clocks);
 
